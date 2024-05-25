a62a4cf48d355b2406f1447a66dd5fd124daf436
hirof <hirofool502@gmail.com>
2024-05-25 18:07:23 +0300

    start work with image

--- src/main/java/ru/skypro/homework/service/ImageService.java
+++ src/main/java/ru/skypro/homework/service/ImageService.java
@@ -1,4 +1,22 @@
 package ru.skypro.homework.service;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.transaction.Transactional;
+
+
+@Service
+@Transactional
 public class ImageService {
+
+    @Value("${avatar.image.directory}")
+    private String avatarsDir;
+
+
+
+    public void uploadImage(Long userId, MultipartFile image) {
+        return;
+    }
 }
