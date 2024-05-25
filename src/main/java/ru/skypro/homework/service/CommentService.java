27ee611a67deac012aaaf4bcfd7a9d4e4aba56c8
hirof <hirofool502@gmail.com>
2024-05-25 19:05:31 +0300

    commit

--- src/main/java/ru/skypro/homework/service/CommentService.java
+++ src/main/java/ru/skypro/homework/service/CommentService.java
@@ -3,6 +3,7 @@ package ru.skypro.homework.service;
 import org.springframework.stereotype.Service;
 import ru.skypro.homework.dto.CreateOrUpdateComment;
 import ru.skypro.homework.entity.Comment;
+import ru.skypro.homework.entity.Comments;
 import ru.skypro.homework.repository.CommentRepository;
 
 @Service
@@ -15,6 +16,17 @@ public class CommentService {
     }
 
     public Comment addComment(int adId, CreateOrUpdateComment comment) {
+        return null;
+    }
+
+    public Comments getComments(int adId) {
+        return null;
+    }
+
+    public void deleteComment(int adId, int commentId) {
+    }
+
+    public Comment updateComment(int adId, int commentId, CreateOrUpdateComment comment) {
         return null;
     }
 }
