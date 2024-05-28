1464702d94633fd3b538e3568e477f786a0855e2
hirof <hirofool502@gmail.com>
2024-05-28 15:23:55 +0300

    commit

--- src/main/java/ru/skypro/homework/dto/CreateOrUpdateAd.java
+++ src/main/java/ru/skypro/homework/dto/CreateOrUpdateAd.java
@@ -1,10 +1,13 @@
 package ru.skypro.homework.dto;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.Size;
+//import javax.validation.constraints.Max;
+//import javax.validation.constraints.Min;
+//import javax.validation.constraints.Size;
 
 @Data
 public class CreateOrUpdateAd {
