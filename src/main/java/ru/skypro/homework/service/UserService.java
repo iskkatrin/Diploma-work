b28776e9043a5f3ebe8e65f87ca635d8b5c3d0da
katerina <klarina2409@gmail.com>
2024-05-29 13:49:36 +0300

    UserService	и ImageController

--- src/main/java/ru/skypro/homework/service/UserService.java
+++ src/main/java/ru/skypro/homework/service/UserService.java
@@ -1,7 +1,10 @@
 package ru.skypro.homework.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ru.skypro.homework.entity.User;
+import ru.skypro.homework.exceptions.UserNotFoundException;
+import ru.skypro.homework.repository.UserRepository;
 
 import javax.transaction.Transactional;
 
@@ -9,8 +12,15 @@ import javax.transaction.Transactional;
 @Transactional
 public class UserService {
 
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     public User findUser(Long userId) {
-        return null;
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new UserNotFoundException("User not found"));
     }
 }
