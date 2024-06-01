f19f5a556db299856f2579d2a3b7755227e03174
katerina <klarina2409@gmail.com>
2024-06-01 22:26:15 +0300

    CommentService

--- src/main/java/ru/skypro/homework/entity/Comment.java
+++ src/main/java/ru/skypro/homework/entity/Comment.java
@@ -34,6 +34,5 @@ public class Comment {
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
-
 }
 
--- src/main/java/ru/skypro/homework/service/CommentService.java
+++ src/main/java/ru/skypro/homework/service/CommentService.java
@@ -5,10 +5,12 @@ import ru.skypro.homework.dto.CreateOrUpdateComment;
 import ru.skypro.homework.entity.Comment;
 import ru.skypro.homework.entity.Comments;
 import ru.skypro.homework.entity.User;
+import ru.skypro.homework.exceptions.CommentNotFoundException;
 import ru.skypro.homework.repository.CommentRepository;
 
 import javax.persistence.EntityNotFoundException;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CommentService {
@@ -20,19 +22,30 @@ public class CommentService {
     }
 
     public Comment addComment(int adId, CreateOrUpdateComment comment) {
-        return null;
+        Comment newComment = new Comment();
+        newComment.setAdId(adId);
+        newComment.setText(comment.getText());
+        return commentRepository.save(newComment);
     }
 
     public List<Comment> getComments(int adId) {
         return commentRepository.findByAdId(adId);
     }
 
-
-
     public void deleteComment(Long commentId) {
         commentRepository.deleteById(commentId);
     }
-}
 
+    public Comment updateComment(int adId, int commentId, CreateOrUpdateComment comment) {
+        Optional<Comment> existingComment = commentRepository.findByCommentIdAndAdId(commentId, adId);
+        if (existingComment.isPresent()) {
+            Comment updatedComment = existingComment.get();
+            updatedComment.setText(comment.getText());
+            return commentRepository.save(updatedComment);
+        } else {
+            throw new CommentNotFoundException("Comment not found with adId: " + adId + " and commentId: " + commentId);
+        }
+    }
+}
 
 
