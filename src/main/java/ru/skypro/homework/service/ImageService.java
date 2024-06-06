92845996c342972c2a9a50032475e57f11c3515f
hirof <hirofool502@gmail.com>
2024-06-06 18:07:46 +0300

    working

--- src/main/java/ru/skypro/homework/service/ImageService.java
+++ src/main/java/ru/skypro/homework/service/ImageService.java
@@ -35,7 +35,7 @@ public class ImageService {
     private final ImageRepository imageRepository;
 
     @Autowired
-    public ImageService(UserService userService, ImageRepository imageRepository) {
+    public ImageService(ImageRepository imageRepository) {
 //        this.userService = userService;
         this.imageRepository = imageRepository;
     }
