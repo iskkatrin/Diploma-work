e3ac0da4d7e2a2b57fd09f89d555778735c1fe54
katerina <klarina2409@gmail.com>
2024-06-08 21:31:13 +0300

    JavaDoc-комментарии

--- src/main/java/ru/skypro/homework/service/ImageService.java
+++ src/main/java/ru/skypro/homework/service/ImageService.java
@@ -7,9 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import ru.skypro.homework.entity.AdEntity;
 import ru.skypro.homework.entity.ImageEntity;
 import ru.skypro.homework.entity.UserEntity;
 import ru.skypro.homework.exceptions.NotSaveAvatarEx;
+import ru.skypro.homework.exceptions.NotSaveImageEx;
 import ru.skypro.homework.repository.ImageRepository;
 
 
@@ -18,6 +20,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Objects;
 
+import static io.swagger.v3.core.util.AnnotationsUtils.getExtensions;
 import static java.nio.file.StandardOpenOption.CREATE_NEW;
 
 
@@ -38,14 +41,6 @@ public class ImageService {
         this.imageRepository = imageRepository;
     }
 
-    /**
-     * saving new user photo
-     * @param userEntity user
-     * @param image new user image
-     * @return new user for saving in userService method
-     * @throws IOException - in working with file
-     */
-    public UserEntity uploadUserImage(UserEntity userEntity, MultipartFile image) throws IOException {
     //Загружает изображение для пользователя по его ID.
     public void uploadImage(Long userId, MultipartFile image) throws IOException {
         UserEntity userEntity = userService.getUserById(userId); //поиск нужного студента
@@ -53,52 +48,6 @@ public class ImageService {
         Path filePath;
         try {
             filePath = Path.of(avatarsDir, userEntity + "." + getExtensions(Objects.requireNonNull(image.getOriginalFilename())));
-        } catch (Exception e) {
-            throw new NotSaveImageEx("ошибка сохранения фотографии в БД");
-        }
-        Files.createDirectories(filePath.getParent());
-        Files.deleteIfExists(filePath);
-
-        try (
-                InputStream is = image.getInputStream();
-                OutputStream os = Files.newOutputStream(filePath, CREATE_NEW);
-                BufferedInputStream bis = new BufferedInputStream(is, 1024);
-                BufferedOutputStream bos = new BufferedOutputStream(os, 1024);
-        ) {
-            bis.transferTo(bos);
-        }
-
-
-        ImageEntity newImageEntity;
-        newImageEntity = imageRepository.findImageByFilePath(filePath.toString());
-
-
-        if (newImageEntity == null) {
-            newImageEntity = new ImageEntity();
-        }
-        newImageEntity.setImageId(userEntity.getUserId());
-        newImageEntity.setData(image.getBytes());
-        newImageEntity.setFilePath(filePath.toString());
-        newImageEntity.setMediaType(image.getContentType());
-        newImageEntity.setFileSize(image.getSize());
-
-        imageRepository.save(newImageEntity);
-        userEntity.setImageEntity(newImageEntity);
-        return userEntity;
-    }
-
-    /**
-     * saving new ad photo
-     * @param adEntity ad
-     * @param image new ad image
-     * @return new ad for saving in adService method
-     * @throws IOException - in working with file
-     */
-    public AdEntity uploadAdImage(AdEntity adEntity, MultipartFile image) throws IOException {
-
-        Path filePath;
-        try {
-            filePath = Path.of(avatarsDir, adEntity + "." + getExtensions(Objects.requireNonNull(image.getOriginalFilename())));
         } catch (Exception e) {
             throw new NotSaveImageEx("ошибка сохранения фотографии в БД");
         }
@@ -115,10 +64,8 @@ public class ImageService {
             bis.transferTo(bos);
         }
 
-
         ImageEntity newImageEntity;
         newImageEntity = imageRepository.findImageByFilePath(filePath.toString());
-
 
         if (newImageEntity == null) {
             newImageEntity = new ImageEntity();
@@ -139,3 +86,4 @@ public class ImageService {
         return extentions;
     }
 }
+
